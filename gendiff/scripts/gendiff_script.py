18c5641e1f74d128df53daa489f83bc7d695c68d
Iurii_Young <yura.young2002@gmail.com>
2024-08-25 18:19:35 +0300

    1 step

---
+++ gendiff/scripts/gendiff_script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def start_gendiff():
+    from gendiff import gendiff_pars
+    gendiff_pars()
+    
+
+
+def main():
+    start_gendiff()
+
+
+if __name__ == '__main__':
+    main()
